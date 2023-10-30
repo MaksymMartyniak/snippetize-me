d7685debdbfc50e2cb7d3dc20bce8dc2ef1fc367
temp-scrt-x0021 <maxmartynyak@gmail.com>
2023-10-30 11:48:38 +0200

    Google auth feature

--- snippetize_me/urls.py
+++ snippetize_me/urls.py
@@ -18,5 +18,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
+    path(
+        'accounts/google/login/callback/',
+        include('allauth.socialaccount.providers.google.urls')
+    ),
 ]
