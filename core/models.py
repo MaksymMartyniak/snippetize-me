ebf90221b01e8d8391d422a4f20f38382c0494e5
mmartyniak <mmartyniak@outwardinc.com>
2023-11-11 22:44:00 +0200

    Added new models

--- core/models.py
+++ core/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from users.models import User
+
 
 class ProgrammingLanguage(models.Model):
     name = models.CharField(max_length=64)
@@ -24,3 +26,45 @@ class Option(models.Model):
     framework = models.ForeignKey(
         Framework, null=True, blank=True, on_delete=models.CASCADE
     )
+
+
+class Thread(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=128)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Prompt(models.Model):
+    thread = models.ForeignKey(Thread, on_delete=models.CASCADE)
+    language = models.ForeignKey(ProgrammingLanguage, on_delete=models.CASCADE)
+    framework = models.ForeignKey(
+        Framework, null=True, blank=True, on_delete=models.CASCADE
+    )
+    option = models.ForeignKey(
+        Option, null=True, blank=True, on_delete=models.CASCADE
+    )
+    # limited to the CharField due to the openai api tokens limit per min
+    # 1 word eq around 2 tokens
+    # the prompt will contain around 320 words = 640 tokens
+    # TPM (tokens per min) = 10 000 -> 15 prompts (requests)
+    prompt_text = models.CharField(max_length=2048)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Response(models.Model):
+    prompt = models.ForeignKey(Prompt, on_delete=models.CASCADE)
+    original_text = models.TextField(blank=True, null=True)
+    first_snippet = models.TextField(blank=True, null=True)
+    second_snippet = models.TextField(blank=True, null=True)
+    comparison_text = models.TextField(blank=True, null=True)
+    api_response_id = models.CharField(max_length=128)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class ResponseStatus(models.Model):
+    response = models.ForeignKey(Response, on_delete=models.CASCADE)
+    status = models.IntegerField()
+    err_msg = models.TextField()  # maybe this should be JSON
